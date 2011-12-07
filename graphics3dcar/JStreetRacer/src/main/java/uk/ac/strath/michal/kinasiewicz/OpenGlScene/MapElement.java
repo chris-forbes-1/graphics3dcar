4944c81945e3a435929911aeea36ff3d269ec09d
mikiones@gmail.com <mikiones@gmail.com@e2219482-4e12-54cf-21d6-893358e7589a>
2011-12-07 13:23:51 +0000

    added missing obj files

--- graphics3dcar/JStreetRacer/src/main/java/uk/ac/strath/michal/kinasiewicz/OpenGlScene/MapElement.java
+++ graphics3dcar/JStreetRacer/src/main/java/uk/ac/strath/michal/kinasiewicz/OpenGlScene/MapElement.java
@@ -2,6 +2,8 @@ package uk.ac.strath.michal.kinasiewicz.OpenGlScene;
 
 import org.lwjgl.util.vector.Vector3f;
 
+import uk.ac.strath.michal.kinasiewicz.OpenGlScene.GLObjects.AbstractNamedGLObject;
+
 /**
  * The each object on map is stored in two parts graphical model 'phisical part'
  * and logical Duo to several logical models can share this same graphical
@@ -15,5 +17,18 @@ public class MapElement {
 	 * Cordinates where the element is on map
 	 */
 	private Vector3f cors;
+	/**
+	 * Angles ax,ay,az for rotatings 
+	 */
 	private Vector3f angels;
+	
+	/**
+	 * reference to GLObject 
+	 */
+	private AbstractNamedGLObject gptr;
+	
+	
+	
+	
+	
 }
